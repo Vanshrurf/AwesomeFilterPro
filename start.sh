2bdcf553fb6c0ecb4a6eafd25de63c84d0fa884b
Vanshrurf <151814401+Vanshrurf@users.noreply.github.com>
2023-12-18 17:56:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sankar8098/Awesomefilteepro-sk /EvaMaria
+  git clone https://github.com/Vanshrurf/AwesomeFilterPro.git /AwesomeFilterPro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /AwesomeFilterPro
 fi
-cd /EvaMaria
+cd /AwesomeFilterPro
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
